b13af7f1e4047d58d5018394f5555fd4f474f64d
rye761 <ryebread761@gmail.com>
2014-02-06 18:11:55 -0600

    Bad Icon apparently, couldn't create icon. Been asking questions, but can't seem to solve :(

--- src/ryebread761/tickertoast/TickerToast.java
+++ src/ryebread761/tickertoast/TickerToast.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.XModuleResources;
+import android.content.res.XResources;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.util.Pair;
@@ -14,19 +15,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 
-public class TickerToast implements IXposedHookZygoteInit {
+public class TickerToast implements IXposedHookZygoteInit, IXposedHookInitPackageResources {
 	ArrayList<Pair<String, String>> toastsShowing;
+	ArrayList<Pair<XResources, String>> packageResources;
 	XModuleResources modRes;
 	int x=0;
 
 	@Override
-	public void initZygote(StartupParam startupParam) throws Throwable {
+	public void initZygote(final StartupParam startupParam) throws Throwable {
 		toastsShowing = new ArrayList<Pair<String, String>>();
+		packageResources = new ArrayList<Pair<XResources,String>>();
 		modRes = XModuleResources.createInstance(startupParam.modulePath, null);
 		XposedHelpers.findAndHookMethod(Toast.class, "show", new XC_MethodReplacement() {
 			
@@ -48,10 +53,22 @@ public class TickerToast implements IXposedHookZygoteInit {
 				final NotificationManager nm = (NotificationManager) toastContext.getSystemService(Context.NOTIFICATION_SERVICE);
 				PackageManager pm = toastContext.getPackageManager();
 				BitmapDrawable toastBD = (BitmapDrawable) pm.getApplicationIcon(toastContext.getPackageName());
+				XResources res = null;
+				for (Pair<XResources, String> aPackage : packageResources) {
+					if (aPackage.second.equals(toastContext.getPackageName())) {
+						res = aPackage.first;
+					}
+				}
+				
+				if (res == null) {
+					//Couldn't get XResources, just show original toast.
+					return XposedBridge.invokeOriginalMethod(param.method, param.thisObject, param.args);
+				}
+				
 				final Notification n = new Notification.Builder(toastContext)
 				.setContentText(toastTextView.getText())
-				.setSmallIcon(modRes.fwd(R.drawable.ic_notif).getId())
 				.setLargeIcon(toastBD.getBitmap())
+				.setSmallIcon(res.addResource(toastContext.getResources(), R.drawable.ic_notif))
 				.setContentTitle(toastContext.getPackageName())
 				.setTicker(toastTextView.getText()).build();
 				final int thisId = x;
@@ -102,6 +119,13 @@ public class TickerToast implements IXposedHookZygoteInit {
 			}
 		}
 		return null;
+	}
+
+
+	@Override
+	public void handleInitPackageResources(InitPackageResourcesParam resparam)
+			throws Throwable {
+		packageResources.add(new Pair<XResources, String>(resparam.res, resparam.packageName));
 	}
 
 }
